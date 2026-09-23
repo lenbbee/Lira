7e8626fd601bc25824ea3000882c29e0ee6e2197
lenbbee <alvealbelen2004@gmail.com>
2026-09-23 19:31:56 -0300

    Crear carpeta data si no existe y limpiar comentarios

--- src/data_manager.py
+++ src/data_manager.py
@@ -34,15 +34,15 @@ def descargar_datos_nasa() -> list:
 
 def obtener_exoplanetas() -> list:
     try: 
-        if os.path.exists(CACHE_FILE): #abres el archivo para ver si esta guardado
-            with open(CACHE_FILE, "r") as f: #explicame esta linea
-                datos = json.load(f) #se escribe en formato json
-                return datos # se retorna los datos
+        if os.path.exists(CACHE_FILE): 
+            with open(CACHE_FILE, "r") as f:
+                datos = json.load(f) 
+                return datos 
         else:
-            datos = descargar_datos_nasa() #se desdcargan los datos
-            with open (CACHE_FILE, "w") as f: #explicmame esta linea que es w y f
-                json.dump(datos, f, indent=4) #se empujan a los datos, aunq explicame esta linea
-            return datos
+            datos = descargar_datos_nasa() 
+            os.makedirs("data", exist_ok=True)
+            with open (CACHE_FILE, "w") as f: 
+                json.dump(datos, f, indent=4) 
     except Exception as e: #para cerrar el try
         print(f"Error al obtener los datos: {e}")
         return []
@@ -57,27 +57,19 @@ def filtrar_habitables(exoplanetas: list) -> list:
         temperatura = planeta.get("pl_eqt")
         densidad = planeta.get("pl_dens")
 
-# 2. Control de calidad para los datos que a veces vienen vacíos (None)
-        # Si la temperatura o la densidad son None, les asignamos un valor por defecto
-        # o una estimación teórica para que tu lógica de filtros no colapse.
         if temperatura is None:
-            temperatura = 250.0  # Estimación teórica templada por defecto
-            
+            temperatura = 250.0 
         if densidad is None:
-            densidad = 4.0  # Estimación de densidad rocosa por defecto
-            
-        # 3. Aplicamos tus filtros científicos de la Fase 1
-        # Distancia <= 200, Temperatura entre 200 y 300, Densidad entre 3.5 y 5.5
+            densidad = 4.0 
+        
         if (distancia <= 200.0) and (200.0 <= temperatura <= 300.0) and (3.5 <= densidad <= 5.5):
             
-            # Si pasa todas las pruebas, lo guardamos en un nuevo diccionario limpio
-            # Al que además le agregamos una etiqueta para que tu interfaz sepa qué tipo de planeta es
             planeta_filtrado = {
                 "name": nombre,
                 "dist": distancia,
                 "temp": temperatura,
                 "dens": densidad,
-                "tipo": "Rocoso Habitable"  # Esta etiqueta servirá para el Pixel Art después
+                "tipo": "Rocoso Habitable"  
             }
             planetas_habitables.append(planeta_filtrado)
             
